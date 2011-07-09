26bb94872dee0c22b6dc3f962c27f66b7c9e11c6
dixon_ <dick.olsson@nodeone.se>
2011-07-09 03:53:59 +0300

    First somewhat working version of the new refactored API

--- uuid.api.php
+++ uuid.api.php
@@ -1,7 +1,8 @@
 <?php
+
 /**
  * @file
- *   Hooks provided by the UUID module.
+ * Hooks provided by the UUID module.
  */
 
 /**
@@ -10,16 +11,16 @@
  * @return
  *   An associative array with the key being the machine name for the
  *   implementation and the values being an array with the following keys:
- *     - callback: The function to be called for generating the UUID.
  *     - title: The human readable name for the generator.
+ *     - callback: The function to be called for generating the UUID.
  *
- * @see uuid_info()
+ * @see uuid_get_info()
  */
 function hook_uuid_info() {
   $generators = array();
   $generators['my_module'] = array(
-    'callback' => 'my_module_generate_uuid',
     'title' => t('My module UUID generator'),
+    'callback' => 'my_module_generate_uuid',
   );
   return $generators;
 }
@@ -28,29 +29,10 @@ function hook_uuid_info() {
  * Ensures all records have a UUID assigned to them.
  *
  * When called this hook should ensure all records it is responsible for
- * have a UUID and if not create one.  The uuid_sync() helper function is
- * available to make this easy to implement.
+ * have a UUID and if not create one.
  *
- * @see uuid_sync()
+ * @see entity_uuid_sync()
  */
 function hook_uuid_sync() {
-  uuid_sync('my_table', 'id');
-}
-
-/**
- * Find an entity using the UUID assigned to it.
- *
- * This is not a hook, but provides a simple way to expose UUID lookups from
- * within a module.
- *
- * @param
- *   $uuid The UUID to use for the lookup.
- *
- * @return
- *   The id field value for the entity or FALSE if not found.
- *
- * @see uuid_find()
- */
-function uuid_TYPE_find($uuid) {
-  return uuid_find($uuid, 'my_table', 'id');
+  // Do what you need to do to generate missing UUIDs for you implementation.
 }
